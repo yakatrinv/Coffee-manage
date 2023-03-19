5226183bfe6b9f45d19d99a234d83a12020f3a78
yasus <yakatrinv@gmail.com>
2023-03-19 14:25:21 +0300

    add test hibernate connection

--- src/test/java/it/academy/util/HibernateUtilTest.java
+++ src/test/java/it/academy/util/HibernateUtilTest.java
@@ -1,16 +1,21 @@
 package it.academy.util;
 
+import org.hibernate.internal.SessionImpl;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class HibernateUtilTest {
     @Test
     public void TestConnection() {
         EntityManager entityManager = HibernateUtil.getEntityManager();
         assertNotNull(entityManager);
+        entityManager.close();
         HibernateUtil.closeFactory();
+        assertTrue(((SessionImpl) entityManager).isClosed());
     }
-  
+
 }
